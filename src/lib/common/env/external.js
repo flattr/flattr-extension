586f3c4ddac27f890f4e17261467c8ddede35171
Thomas Greiner <thomas@greinr.com>
2018-02-16 17:51:32 +0100

    Noissue - Exposed functions for modifying account details

--- src/lib/common/env/external.js
+++ src/lib/common/env/external.js
@@ -1,5 +1,8 @@
 "use strict";
 
+const account = require("../account");
 const {dailyProcessing} = require("../../background/history/task");
 
 exports.dailyProcessing = dailyProcessing;
+exports.setSubscription = account.setSubscription;
+exports.setToken = account.setToken;
