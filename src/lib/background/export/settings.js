e88882a2ffd19bb45a3d38bbdabc91bd67e52528
Erik Vold <erik@evold.ca>
2018-06-10 23:01:43 -0700

    filter out the auth token

--- src/lib/background/export/settings.js
+++ src/lib/background/export/settings.js
@@ -6,7 +6,11 @@ function getExtensionSettings()
 {
   return new Promise((resolve) =>
   {
-    chrome.storage.local.get(null, resolve);
+    chrome.storage.local.get(null, (settings) =>
+    {
+      delete settings["account.token"];
+      resolve(settings);
+    });
   });
 }
 exports.getExtensionSettings = getExtensionSettings;
